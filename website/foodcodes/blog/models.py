b64b3e722e94df7ab85a1a2356d83bdb878576c7
Pratik Sharma <55416418+pratiksharma8@users.noreply.github.com>
2020-05-04 20:24:49 -0700

    Update models.py

--- website/foodcodes/blog/models.py
+++ website/foodcodes/blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
-from imagekit.models import ImageSpecField
+from imagekit.models import ImageSpecField, ProcessedImageField
 from imagekit.processors import ResizeToFit
 
 
@@ -10,12 +10,17 @@ from imagekit.processors import ResizeToFit
 
 class Post(models.Model):
     title = models.CharField(max_length=50)
-    photo = models.ImageField(upload_to='photos', blank=False)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     likes = models.ManyToManyField(User, related_name='likes')
 
+    photo = ProcessedImageField(upload_to='photos',
+                                processors=[ResizeToFit(1000, 800)],
+                                format='JPEG',
+                                options={'quality': 100},
+                                blank=False)
+    
     blog_photo = ImageSpecField(source='photo',
                                 processors=[ResizeToFit(600, 700)],
                                 format='JPEG',
